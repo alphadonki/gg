9fdb9d4b487b39acdaffefea8036d7cef24882b8
alphadonki <107272180+alphadonki@users.noreply.github.com>
2022-08-15 18:39:32 +0300

    Add files via upload

---
+++ pages/[copy].tsx
@@ -0,0 +1,217 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Script from 'next/script'
+
+const Home: NextPage = () => {
+  return (
+<>
+
+<Head>
+  <meta charSet="UTF-8" />
+  <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="apple-touch-fullscreen" content="yes" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+  <title>AlphaDonki | Home</title>
+  <meta name="description" content="AlphaDonki | are u alpha enuogh?" />
+  <meta
+    name="viewport"
+    content="width=device-width, height=device-height, initial-scale=1, user-scalable=no"
+  />
+  <meta name="msapplication-TileColor" content="#000000" />
+  <meta name="msapplication-TileImage" content="/images/ms-icon-144x144.png" />
+  <meta name="theme-color" content="#000000" />
+  <meta property="og:image" content="" />
+  <meta property="og:title" content="AlphaDonki" />
+  <meta property="og:description" content=" AlphaDonki | Are u alpha enough?" />
+  <meta property="og:site_name" content="AlphaDonki" />
+  
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="../public/assets/fav/favicon-32x32.png"/>
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="../public/assets/fav/favicon-16x16.png"
+  />
+  <link rel="manifest" href="/assets/fav/site.webmanifest" />
+  <meta name="msapplication-TileColor" content="#da532c" />
+  <meta name="theme-color" content="#ffffff" />
+</Head>
+
+<div className="bubbles_wrap">
+        <div className="bubble x1" />
+        <div className="bubble x2" />
+        <div className="bubble x3" />
+        <div className="bubble x4" />
+        <div className="bubble x5" />
+        <div className="bubble x6" />
+        <div className="bubble x7" />
+        <div className="bubble x8" />
+        <div className="bubble x9" />
+        <div className="bubble x10" />
+      </div>
+      <div className="loader">
+        <img src="logo-intro.png" alt="Loading..." />
+      </div>
+
+
+
+<div className="cloud-bg">
+    <div className="lottie-animation" />
+  </div>
+  <div className="global-styles w-embed">
+</div>
+  <div className="background"> </div>
+  <div
+    data-collapse="medium"
+    data-animation="over-left"
+    data-duration={400}
+    data-w-id="f64aa1bf-a3b0-9533-c762-1410582692c6"
+    data-easing="ease"
+    data-easing2="ease"
+    role="banner"
+    className="navbar3_component navbar w-nav"
+  >
+    <div className="navbar3_container">
+      <div
+        id="w-node-f64aa1bf-a3b0-9533-c762-1410582692c8-582692c6"
+        className="navbar3_menu-button w-nav-button"
+      >
+        <div className="menu-icon3">
+          <div className="menu-icon3_line-top" />
+          <div className="menu-icon3_line-middle" />
+          <div className="menu-icon3_line-bottom" />
+        </div>
+      </div>
+      <nav
+        role="navigation"
+        id="w-node-f64aa1bf-a3b0-9533-c762-1410582692cd-582692c6"
+        className="navbar3_menu w-nav-menu"
+      >
+        <a href="#" className="navbar3_logo-link-menu w-nav-brand"></a>
+        <a className="navbar3_link_not left-mobile">STAKING</a>
+        <a className="navbar3_link_not left-mobile">RAFFLES</a>
+        <a className="navbar3_link_not left-mobile">SHOP</a>
+        <div
+          data-hover="true"
+          data-delay={200}
+          data-w-id="f64aa1bf-a3b0-9533-c762-1410582692d6"
+          className="navbar3_menu-dropdown w-dropdown"
+        >
+          <div className="navbar3_dropdown-toggle w-dropdown-toggle">
+            <div className="dropdown-icon w-icon-dropdown-toggle" />
+            <div>Link Three</div>
+          </div>
+          <nav className="navbar3_dropdown-list w-dropdown-list">
+            <a href="#" className="navbar3_dropdown-link w-dropdown-link">
+              Link Four
+            </a>
+            <a href="#" className="navbar3_dropdown-link w-dropdown-link">
+              Link Five
+            </a>
+            <a href="#" className="navbar3_dropdown-link w-dropdown-link">
+              Link Six
+            </a>
+          </nav>
+        </div>
+        <a href="#" className="navbar3_tablet-menu-button w-button">
+          Button
+        </a>
+      </nav>
+      <a
+        href="#"
+        id="w-node-f64aa1bf-a3b0-9533-c762-1410582692e4-582692c6"
+        aria-current="page"
+        className="navbar3_logo-link w-nav-brand w--current"
+      >
+      
+      </a>
+      <nav
+        role="navigation"
+        id="w-node-f64aa1bf-a3b0-9533-c762-1410582692e6-582692c6"
+        className="navbar3_menu w-nav-menu"
+      >
+        <a
+          href="#"
+          aria-current="page"
+          className="mobile-nav-brand-block margin-bottom margin-medium w-inline-block w--current"
+        >
+          <div
+            className="mobilenav-menu-title margin-left margin-medium"
+            style={{ color: "rgb(40, 40, 40)" }}
+          >
+            {" "}
+            AlphaDonki
+          
+          </div>
+        </a>
+        <a className="navbar3_link_not left-mobile">TEAM</a>
+        <a className="navbar3_link_not left-mobile ">ALPHAMAP</a>
+        <a className="navbar3_link_not left-mobile ">ALPHAPAPER</a>
+        <a href="#" className="navbar3_tablet-menu-button w-button">
+          Button
+        </a>
+      </nav>
+      <div className="navbar3_menu-background" />
+    </div>
+  </div>
+  <div className="page-wrapper landing">
+    <main className="main-wrapper-landing">
+      <div className="div-block-5">
+        <div className="div-block-4">
+          <div className="div-block-19">
+            <p> Are u Alpha Enough?</p>
+            <h1 className="landing-heading">ALPHA</h1>
+            <h1>DONKI.. </h1>
+            <div className="div-block-9"></div>
+          </div>
+        </div>
+        <div className="div-block">
+          <a href="https://twitter.com/alphadonki"
+            rel="opener"
+            className="social-link w-inline-block"
+          >
+            
+          </a>
+          <a href="https://discord.com/invite/ZpJ44GtWj9"
+            rel="noopener"
+            className="social-link w-inline-block">
+            
+          </a>
+        </div>
+      </div>
+    </main>
+    <div className="div-block-3">
+      
+      <div className="w-layout-grid footer4_bottom-wrapper home-page">
+        <a
+          href="#"
+          aria-current="page"
+          className="footer4_legal-link w--current"
+        >
+          © 2022 AlphaDonki
+        </a>
+        <a className="footer4_legal-link">We are Alphas</a>
+        <a
+          href="https://solana.com/"
+          rel="noopener"
+          className="footer4_legal-link"
+        >
+          build on Solana{" "}</a>
+      </div>
+    </div>
+  </div>
+
+
+<Script src="/1.js" type="text/javascript"></Script>
+<Script src="/2.js" type="text/javascript"></Script>
+
+</>)}
+export default Home
